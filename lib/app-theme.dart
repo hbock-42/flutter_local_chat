9ac37e7d075d9039154995787b10fdcc6f3c88bb
Hugo Bock <h.bock@easyrecrue.com>
2020-05-06 19:36:22 +0000

    message design start

--- lib/app-theme.dart
+++ lib/app-theme.dart
@@ -4,6 +4,11 @@ class AppTheme {
   static const Color backgroundColor = Color(0xFF000000);
   static const Color widgetBackgroundColor = Color(0xFF383C40);
   static const Color textColor = Color(0xFFFFFFFF);
+  static const Gradient widgetBackroundGradient = LinearGradient(
+    colors: [Color(0xFF607EFF), Color(0xFFC66AF7)],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+  );
 
   static const TextStyle mainText =
       TextStyle(fontFamily: "Poppins", color: textColor);
--- lib/widgets/message-widget.dart
+++ lib/widgets/message-widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:local_chat/app-theme.dart';
 import 'package:local_chat/models/message.dart';
 
 class MessageWidget extends StatelessWidget {
@@ -12,6 +13,33 @@ class MessageWidget extends StatelessWidget {
         alignment: message.type == MessageType.Received
             ? Alignment.topLeft
             : Alignment.topRight,
-        child: Text(message.content));
+        child: _messageContainer(message));
+  }
+
+  Widget _messageContainer(Message message) {
+    double radiusValue = 10;
+    var decoration = message.type == MessageType.Received
+        ? BoxDecoration(
+            color: AppTheme.widgetBackgroundColor,
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(radiusValue),
+              bottomRight: Radius.circular(radiusValue),
+              bottomLeft: Radius.circular(radiusValue),
+            ),
+          )
+        : BoxDecoration(
+            gradient: AppTheme.widgetBackroundGradient,
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(radiusValue),
+              topLeft: Radius.circular(radiusValue),
+              bottomLeft: Radius.circular(radiusValue),
+            ),
+          );
+
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+      decoration: decoration,
+      child: Text(message.content),
+    );
   }
 }
